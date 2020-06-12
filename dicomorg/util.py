d322548cbf36d911d17202ea31e59f20e41287ac
Joshua Teves <jbtevespro@gmail.com>
2020-06-12 15:12:09 -0400

    Adds group by attribute-value

--- dicomorg/util.py
+++ dicomorg/util.py
@@ -2,7 +2,7 @@
 # -*- coding : utf-8 -*-
 
 def group_key_att(indict, attribute, subset=None, sort=True):
-    """Groups keys by the attribute, sorting the attribute
+    """Group keys by the attribute, sorting the attribute
 
     Parameters
     ----------
@@ -71,3 +71,61 @@ def group_key_att(indict, attribute, subset=None, sort=True):
         groups = [x for _,x in sorted(zip(unique_values, groups))]
 
     return groups, unique_values
+
+
+def group_key_val(indict, attribute, target_value, subset=None):
+    """Return a group of keys which match the desired attribute and value.
+
+    Parameters
+    ----------
+    indict : dict
+        The input dictionary to search through.
+    attribute : str
+        The attribute to examine for each key.
+    target_value : any
+        The value you'd like to know which keys match.
+    subset : list
+        The subset of keys you'd like to explore. Default: None.
+
+    Returns
+    -------
+    A list of keys which have the desired match.
+
+    Raises
+    ------
+    AttributeError
+        If one of the items keyed does not have the desired attribute.
+    KeyError
+        If one of the keys in the subset is not present in the dict.
+    TypeError
+        If the type of a parameter types are incorrect
+    """
+    # Type checking
+    if not isinstance(indict, dict):
+        raise TypeError('indict is not a dict')
+    if not isinstance(attribute, str):
+        raise TypeError('attribute is not a string')
+    if subset:
+        if not isinstance(subset, list):
+            raise TypeError('subset is not a list')
+        # Check list is not empty
+        if len(subset) == 0:
+            subset = [k for k in indict]
+        else:
+            # Check for key existence
+            extant_keys = [k for k in indict]
+            for k in subset:
+                if not (k in extant_keys):
+                    raise KeyError('Key in subset not present in indict')
+    else:
+        subset = [k for k in indict]
+
+    matching_keys = ['' for i in subset]
+    total_matches = 0
+    for k in subset:
+        current_val = getattr(indict[k], attribute)
+        if current_val == target_value:
+            matching_keys[total_matches] = k
+            total_matches += 1
+
+    return matching_keys[0:total_matches]
--- test/test_util.py
+++ test/test_util.py
@@ -1,13 +1,15 @@
 #/usr/bin/env python
 # -*- coding : utf-8 -*-
 
+import sys
+
 import pytest
 
 from dicomorg import util
 from dicomorg.util import *
 
 class Point:
-    """Simple class for testing
+    """Simple class for testing.
 
     Attributes:
         x : the x-coordinate
@@ -123,3 +125,95 @@ def test_group_key_att():
 
     # Group 1 should have 'dog'
     assert y_groups[1] == ['dog']
+
+
+def test_group_key_value():
+    """Test group_key_value with simple point class."""
+    # Construct some points and put them into dicts
+    testdict = {}
+    testdict['cat'] = Point(1,1)
+    testdict['dog'] = Point(1,2)
+    testdict['zebra'] = Point(3,3)
+
+    # ------------------
+    # Check for failures
+    # ------------------
+    # Should cause error when indict is not a dict
+    with pytest.raises(TypeError, match=r'indict*'):
+        x_groups, x_vals = group_key_val(1, 'x', None)
+
+    # Should cause error when attribute is not a string
+    with pytest.raises(TypeError, match=r'attribute*'):
+        x_groups, x_vals = group_key_val(testdict, 1, None)
+
+    # Should cause error when subset isn't a list
+    with pytest.raises(TypeError, match=r'subset*'):
+        x_groups, x_vals = group_key_val(testdict, 'x', None, subset=1)
+
+    # Should cause error when sort isn't a boolean
+    with pytest.raises(TypeError, match=r'sort*'):
+        x_groups, x_vals = group_key_val(testdict, 'x', None, sort='pi')
+
+    # Check for failure when using non-existent attribute to group
+    with pytest.raises(AttributeError):
+        z_vals, z_groups = group_key_val(testdict, 'z', None)
+
+    # Check for failure when using non-existent subset
+    with pytest.raises(KeyError):
+        x_groups, x_vals = group_key_val(testdict, 'x',
+                                         None, subset=['seal'])
+
+
+    # -----------------------------------
+    # Check for Matches to various values
+    # -----------------------------------
+    # Check that correct ones are hit
+    matches_x_1 = group_key_val(testdict, 'x', 1)
+    assert sorted(matches_x_1) == ['cat', 'dog']
+
+    matches_x_3 = group_key_val(testdict, 'x', 3)
+    assert matches_x_3 == ['zebra']
+
+    matches_y_1 = group_key_val(testdict, 'y', 1)
+    assert matches_y_1 == ['cat']
+
+    matches_y_2 = group_key_val(testdict, 'y', 2)
+    assert matches_y_2 == ['dog']
+
+    matches_y_3 = group_key_val(testdict, 'y', 3)
+    assert matches_y_3 == ['zebra']
+
+    # Check that there is no result for a value that doesn't exist
+    matches_x_naught = group_key_val(testdict, 'x', 0)
+    assert matches_x_naught == []
+
+    matches_x_null = group_key_val(testdict, 'x', None)
+    assert matches_x_null == []
+
+    matches_y_naught = group_key_val(testdict, 'y', 0)
+    assert matches_y_naught == []
+
+    matches_y_null = group_key_val(testdict, 'y', None)
+    assert matches_y_naught == []
+
+    # Check that subset works
+    testdict['philosophy'] = Point(1,-1) # Add an overload to x = 1
+    testdict['afterthought'] = Point(-1,1) # Add an overload to y = 1
+
+    matches_x_subset = group_key_val(testdict, 'x', 1,
+                                     subset=['cat', 'dog'])
+    assert matches_x_subset == ['cat', 'dog']
+
+    # Check that subset works for skipping a match
+    matches_x_subset = group_key_val(testdict, 'x', 1,
+                                     subset=['zebra'])
+    assert matches_x_subset == []
+
+    matches_y_subset = group_key_val(testdict, 'y', 1,
+                                     subset=['cat', 'dog'])
+    assert matches_y_subset == ['cat']
+
+    # Check that subset works for skipping a match
+    matches_y_subset = group_key_val(testdict, 'y', 3,
+                                     subset=['cat', 'dog'])
+    assert matches_x_subset == []
